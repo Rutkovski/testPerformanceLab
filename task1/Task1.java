483179bf60f70b684a518567a38d1459f4216bdc
Rutkovski Dmitriy <rutkovski.dp@gmail.com>
2023-01-14 22:19:37 +0300

    add task1

---
+++ task1/Task1.java
@@ -0,0 +1,15 @@
+public class Task1 {
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]) ;
+        int m = Integer.parseInt(args[1]);
+        int i = 1;
+        System.out.print(i);
+        while (true) {
+            i = 1 + (i + m - 2) % n;
+            if (i == 1) break;
+            System.out.print(i);
+        }
+    }
+
+}
